94083b9865299e28cc7a0244151d8796eb6c7299
uude1 <edu.g.neto06@gmail.com>
2023-10-16 17:04:00 -0300

    main - adicionado campo de datepicker do antid, que tem disabled para anos anteriores aos anos de 2023

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -447,6 +447,77 @@ export default function Dashboard() {
             />
           </Form.Item>
           <Form.Item
+            label="Mês:"
+            name="month"
+            rules={[{ required: true, message: 'Insira o Mes!' }]}
+          >
+            <Select
+              className='select-month'
+              defaultValue="Mês"
+              onChange={(selectedOption) => {
+                if (selectedOption && selectedOption.valueOf()) {
+                  const selectedValue = parseInt(selectedOption.valueOf());
+                  setMonth(selectedValue);
+                }
+              }}
+              options={[
+                { value: "1", label: 'Janeiro' },
+                { value: "2", label: 'Fevereiro' },
+                { value: "3", label: 'Março' },
+                { value: "4", label: 'Abril' },
+                { value: "5", label: 'Maio' },
+                { value: "6", label: 'Junho' },
+                { value: "7", label: 'Julho' },
+                { value: "8", label: 'Agosto' },
+                { value: "9", label: 'Setembro' },
+                { value: "10", label: 'Outubro' },
+                { value: "11", label: 'Novembro' },
+                { value: "12", label: 'Dezembro' },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Ano:"
+            name="year"
+            rules={[{ required: true, message: 'Insira o Ano!' }]}
+          >
+            <Select
+              className='select-year'
+              defaultValue="Ano"
+              onChange={(selectedOptionYear) => {
+                if (selectedOptionYear && selectedOptionYear.valueOf()) {
+                  const selectedValueYear = parseInt(selectedOptionYear.valueOf());
+                  setYear(selectedValueYear);
+                }
+              }}
+              options={[
+                { value: "2018", label: '2018', disabled: true },
+                { value: "2019", label: '2019', disabled: true },
+                { value: "2020", label: '2020', disabled: true },
+                { value: "2021", label: '2021', disabled: true },
+                { value: "2022", label: '2022', disabled: true },
+                { value: "2023", label: '2023' },
+                { value: "2024", label: '2024' },
+                { value: "2025", label: '2025' },
+                { value: "2026", label: '2026' },
+                { value: "2027", label: '2027' },
+                { value: "2028", label: '2028' },
+                { value: "2029", label: '2029' },
+                { value: "2030", label: '2030' },
+                { value: "2031", label: '2031' },
+                { value: "2032", label: '2032' },
+                { value: "2033", label: '2033' },
+                { value: "2034", label: '2034' },
+                { value: "2035", label: '2035' },
+                { value: "2036", label: '2036' },
+                { value: "2037", label: '2037' },
+                { value: "2038", label: '2038' },
+                { value: "2039", label: '2039' },
+                { value: "2040", label: '2040' },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item
             label="Renda Fixa:"
             name="fixedIncome"
             rules={[{ required: true, message: 'Insira a Renda Fixa' }]}
